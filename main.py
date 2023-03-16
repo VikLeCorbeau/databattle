dbe2d6f4db18220b72607ac5ed7703a5c4ac11ce
Baucor Victor <baucor.victor@gmail.com>
2023-03-16 16:44:49 +0100

    ok

--- main.py
+++ main.py
@@ -12,53 +12,45 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Creation du dataset à partir d'images des patterns figurants sur les pdf
 img_height = 32
-img_width = 66
-batch_size = 22
+img_width = 72
+batch_size = 25
 
-# ds_train = tf.keras.preprocessing.image_dataset_from_directory(
-#     'dataset/',
-#     labels = 'inferred',
-#     label_mode = 'int',
-#     color_mode = 'grayscale',
-#     batch_size = batch_size,
-#     image_size = (img_height, img_width),
-#     shuffle = True,
-#     seed = 123,
-#     validation_split = 0.1,
-#     subset = 'training',
-# )
+def train_the_model(h, w, b):
+    ds_train = tf.keras.preprocessing.image_dataset_from_directory(
+        'dataset/',
+        labels = 'inferred',
+        label_mode = 'int',
+        color_mode = 'grayscale',
+        batch_size = batch_size,
+        image_size = (img_height, img_width),
+        shuffle = True,
+        seed = 123,
+        validation_split = 0.1,
+        subset = 'training',
+    )
 
-# ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
-#     'dataset/',
-#     labels = 'inferred',
-#     label_mode = 'int',
-#     color_mode = 'grayscale',
-#     batch_size = batch_size,
-#     image_size = (img_height, img_width),
-#     shuffle = True,
-#     seed = 123,
-#     validation_split = 0.1,
-#     subset = 'validation',
-# )
+    print(ds_train)
 
-# model = tf.keras.models.Sequential()
-# model.add(tf.keras.layers.Flatten(input_shape=(img_height, img_width)))
-# model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-# model.add(tf.keras.layers.Dense(units=256, activation=tf.nn.relu))
-# model.add(tf.keras.layers.Dense(units=batch_size, activation=tf.nn.softmax))
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(h, w)))
+    model.add(tf.keras.layers.Dense(units=1024, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=1024, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=b, activation=tf.nn.softmax))
 
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# model.fit(ds_train, epochs=1000)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(ds_train, epochs=55)
 
-# loss, accuracy = model.evaluate(ds_train)
-# print(accuracy)
-# print(loss) 
+    loss, accuracy = model.evaluate(ds_train)
+    print(accuracy)
+    print(loss) 
 
-# model.save('ground.model')
+    model.save('ground.model')
+
+train_the_model(img_height, img_width, batch_size)
 
 model = tf.keras.models.load_model('ground.model')
 
-for x in range(1, 6):
+for x in range(1, 4):
     img = cv.imread(f'{x}.jpg')[:,:,0]
     img = np.invert(np.array([img]))
 
